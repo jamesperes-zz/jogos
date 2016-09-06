0d7ec497cef92a1e5aab4a42171412fa33e216cd
James <james.peres.s@gmail.com>
2016-09-06 04:06:22 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -5,7 +5,11 @@ print ( 30 * '-')
 print (" \n    Jogo da forca    \n")
 print (30 * '-')
 
-itens_fruta = ["banana", "abacaxi"]
+itens_fruta = ["banana", "abacaxi", "maca"]
+
+fruta = random.choice(itens_fruta)
+
+print (len(fruta))
 
 
 
