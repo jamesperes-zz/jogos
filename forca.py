6d4172c8a605e657e592ba1defe4ff985ebce503
James <james.peres.s@gmail.com>
2016-09-06 04:27:05 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -11,10 +11,15 @@ fruta = random.choice(itens_fruta)
 
 print ("o numero de letras e: %s " %(len(fruta)))
 
+letra_1 = str(input("digite uma letra: "))
+k = 0
+troca = ""
+while k < len(fruta):
+    if letra in fruta:
+        troca = troca + letra
+    else:
+        break
 
-
-
-
-
+print ("%s" %troca)
 
 time.sleep (5)
